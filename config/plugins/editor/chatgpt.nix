052b1809a5c30caf25100ba2565abe5bc045a458
Lin Vieira <lin@fnxln.com>
2024-09-05 15:52:29 -0300

    change token length

--- config/plugins/editor/chatgpt.nix
+++ config/plugins/editor/chatgpt.nix
@@ -3,8 +3,8 @@ _: {
     enable = true;
     settings = {
       openai_params = {
-        model = "gpt-4o";
-        max_tokens = 16380;
+        model = "gpt-4o-2024-08-06";
+        max_tokens = 16384;
       };
       api_key_cmd = "cat ~/.chat_key";
       extra_curl_params = [];
