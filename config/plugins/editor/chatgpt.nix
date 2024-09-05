edeeaa08809821ba5b5201995c8d3cbd9c15fb31
Lin Vieira <lin@fnxln.com>
2024-09-05 16:06:59 -0300

    reduce tokens

--- config/plugins/editor/chatgpt.nix
+++ config/plugins/editor/chatgpt.nix
@@ -4,7 +4,7 @@ _: {
     settings = {
       openai_params = {
         model = "gpt-4o-2024-08-06";
-        max_tokens = 16384;
+        max_tokens = 300;
       };
       api_key_cmd = "cat ~/.chat_key";
       extra_curl_params = [];
