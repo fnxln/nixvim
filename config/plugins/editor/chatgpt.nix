a8b380fabb6f2a497ffb13ac94616c5d3e524afa
Lin Vieira <lin@fnxln.com>
2024-09-05 00:22:16 -0300

    increase tokens

--- config/plugins/editor/chatgpt.nix
+++ config/plugins/editor/chatgpt.nix
@@ -4,6 +4,7 @@ _: {
     settings = {
       openai_params = {
         model = "gpt-4o";
+        max_tokens = 16380;
       };
       api_key_cmd = "cat ~/.chat_key";
       extra_curl_params = [];
