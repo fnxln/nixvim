067e08f34d61af537b9b93ba7619753626281a2d
Lin Vieira <lin@favela.network>
2025-04-06 07:02:38 -0300

    add openai config

--- config/completion/codecompanion.nix
+++ config/completion/codecompanion.nix
@@ -6,7 +6,47 @@
   config = lib.mkIf config.codecompanion.enable {
     plugins.codecompanion = {
       enable = true;
-      luaConfig = { };
+      settings = {
+        adapters = {
+          openai = {
+            __raw = ''
+              function()
+                return require("codecompanion.adapters").extend("openai", {
+                  env = {
+                    url = "cmd:cat ~/.secret/openai",
+                  },
+                  schema = {
+                    model = {
+                      default = "gpt-4o"
+                    }
+                  }
+                })
+              end
+            '';
+          };
+        };
+        strategies = {
+          agent = {
+            adapter = "openai";
+          };
+          chat = {
+            adapter = "openai";
+          };
+          inline = {
+            adapter = "copilot";
+          };
+        };
+        display = {
+          action_palette = {
+            provider = "default";
+          };
+          chat = {
+            window = {
+              layout = "vertical";
+            };
+          };
+        };
+      };
     };
   };
 
